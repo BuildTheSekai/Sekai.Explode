a159f62730f6225d471716ba9d2b2d0686cf09ec
takejohn <105504345+takejohn@users.noreply.github.com>
2024-03-21 19:34:02 +0900

    1列のカレンダー

--- packages/misc/commands/cal.ts
+++ packages/misc/commands/cal.ts
@@ -1,11 +1,34 @@
 import { SimpleSlashCommandBuilder } from '../../../common/SimpleCommand';
+import { Day, MonthCalendar } from '../util/calendar';
 
 export default SimpleSlashCommandBuilder.create(
 	'cal',
 	'カレンダーを表示します',
 ).build(async (interaction) => {
-	await interaction.reply({
-		content: '開発中',
-		ephemeral: true,
-	});
+	const calendar = new MonthCalendar();
+	const dates = [];
+	const size = calendar.size;
+	for (let i = 1; i <= size; i++) {
+		dates.push(`${dayToString(calendar.dayOf(i))} ${String(i).padStart(2)}`);
+	}
+	await interaction.reply('```' + dates.join('\n') + '```');
 });
+
+function dayToString(day: Day) {
+	switch (day) {
+		case Day.Sunday:
+			return 'Su';
+		case Day.Monday:
+			return 'Mo';
+		case Day.Tuesday:
+			return 'Tu';
+		case Day.Wednesday:
+			return 'We';
+		case Day.Thursday:
+			return 'Th';
+		case Day.Friday:
+			return 'Fr';
+		case Day.Saturday:
+			return 'Sa';
+	}
+}
