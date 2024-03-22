7e3c092b4b5004fee3ed0ba7895f9a39584d505f
takejohn <105504345+takejohn@users.noreply.github.com>
2024-03-22 21:10:13 +0900

    言語ファイルを使用

--- packages/misc/commands/cal.ts
+++ packages/misc/commands/cal.ts
@@ -1,9 +1,10 @@
 import { SimpleSlashCommandBuilder } from '../../../common/SimpleCommand';
+import { LANG } from '../../../util/languages';
 import { MonthCalendar } from '../util/calendar';
 
 export default SimpleSlashCommandBuilder.create(
-	'cal',
-	'カレンダーを表示します',
+	LANG.commands.cal.name,
+	LANG.commands.cal.description,
 ).build(async (interaction) => {
 	const calendar = new MonthCalendar();
 	const days = Array.from(calendar.weeks())
@@ -15,5 +16,7 @@ export default SimpleSlashCommandBuilder.create(
 				.join(' '),
 		)
 		.join('\n');
-	await interaction.reply('```Su Mo Tu We Th Fr Sa\n' + days + '```');
+	await interaction.reply(
+		'```' + LANG.commands.cal.dayLabels.join(' ') + '\n' + days + '```',
+	);
 });
