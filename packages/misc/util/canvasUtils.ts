3f010a35e9bb87b4bf8c4ff2c658b9930d7c842d
takejohn <105504345+takejohn@users.noreply.github.com>
2024-03-27 15:09:56 +0900

    均等に配置

--- packages/misc/util/canvasUtils.ts
+++ packages/misc/util/canvasUtils.ts
@@ -25,28 +25,63 @@ export class BoundingBox {
 	}
 }
 
+export class CanvasTextBox {
+	public text: string;
+
+	public boundingBox: BoundingBox;
+
+	public color: string;
+
+	constructor(text: string, boundingBox: BoundingBox) {
+		this.text = text;
+		this.boundingBox = boundingBox;
+	}
+
+	renderTo(ctx: CanvasRenderingContext2D) {
+		ctx.save();
+		ctx.font = '24px serif';
+		ctx.fillStyle = this.color;
+		ctx.textBaseline = 'top';
+		ctx.fillText(this.text, this.boundingBox.x, this.boundingBox.y);
+		ctx.restore();
+	}
+}
+
 export class CanvasTable {
-	private cells: string[][];
+	private cells: CanvasTextBox[][];
 
 	private boundingBox: BoundingBox;
 
 	public color: string;
 
 	constructor(cells: string[][], boundingBox: BoundingBox) {
-		this.cells = cells;
+		const rowCount = cells.length;
+		const columnCount = cells[0].length;
+		const cellWidth = boundingBox.width / columnCount;
+		const cellHeight = boundingBox.height / rowCount;
+		this.cells = cells.map((row, i) =>
+			row.map(
+				(cell, j) =>
+					new CanvasTextBox(
+						cell,
+						new BoundingBox(
+							boundingBox.x + cellWidth * j,
+							boundingBox.y + cellHeight * i,
+							boundingBox.width,
+							boundingBox.height,
+						),
+					),
+			),
+		);
 		this.boundingBox = boundingBox;
 	}
 
 	renderTo(ctx: CanvasRenderingContext2D) {
-		ctx.save();
-		ctx.font = '48px monospace';
-		ctx.fillStyle = this.color;
-		ctx.textBaseline = 'top';
-		ctx.fillText(
-			formatTable(this.cells),
-			this.boundingBox.x,
-			this.boundingBox.y,
-		);
-		ctx.restore();
+		for (const row of this.cells) {
+			for (const cell of row) {
+				cell.color = this.color;
+				cell.renderTo(ctx);
+			}
+		}
 	}
 }
