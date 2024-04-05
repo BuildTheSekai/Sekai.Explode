ee7e83b37a4a7b3a89dde2bf6cddf32e80c67e49
takejohn <105504345+takejohn@users.noreply.github.com>
2024-04-05 19:11:56 +0900

    設定でLokiJSを使用できるように

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,45 +1,86 @@
-import { Client } from 'discord.js';
 import { Feature } from '../../common/Feature';
 import { LANG } from '../../util/languages';
 import mongoose from 'mongoose';
+import Loki from 'lokijs';
 import config from '../../internal/config';
 import { Connection } from './types';
 import { MongoDBWrapper } from './implementations/MongoDB';
+import { LokiJSWrapper } from './implementations/LokiJS';
 
 export * from './types';
-
-const connection = mongoose.connection;
-connection.on('connecting', function () {
-	console.log(LANG.internal.mongodb.dbConnecting);
-});
-connection.on('connected', function () {
-	console.log(LANG.internal.mongodb.dbConnected);
-});
-connection.on('disconnecting', function () {
-	console.log(LANG.internal.mongodb.dbDisconnecting);
-});
-connection.on('disconnected', function () {
-	console.log(LANG.internal.mongodb.dbDisconnected);
-});
 
 class DbFeature extends Feature {
 	name = 'db';
 
-	connection: Connection = new MongoDBWrapper(connection);
+	connection: Connection;
 
-	async onLoad(client: Client<boolean>) {
+	connectionPromise: Promise<void>;
+
+	constructor() {
+		super();
+		console.log('database: ', config.database);
+		switch (config.database) {
+			default:
+			case 'mongo': {
+				const connection = mongoose.connection;
+				connection.on('connecting', function () {
+					console.log(LANG.internal.mongodb.dbConnecting);
+				});
+				connection.on('connected', function () {
+					console.log(LANG.internal.mongodb.dbConnected);
+				});
+				connection.on('disconnecting', function () {
+					console.log(LANG.internal.mongodb.dbDisconnecting);
+				});
+				connection.on('disconnected', function () {
+					console.log(LANG.internal.mongodb.dbDisconnected);
+				});
+				this.connection = new MongoDBWrapper(connection);
+				this.connectionPromise = mongoose
+					.connect(
+						`mongodb://${config.mongoDBuser}:${config.mongoDBpass}@${config.mongoDBhost}:${config.mongoDBport}/${config.mongoDBdatabase}?authSource=admin`,
+					)
+					.then(() => undefined);
+				return;
+			}
+
+			case 'loki': {
+				console.log('Loading Loki');
+				let resolve!: (value: void | PromiseLike<void>) => void;
+				let reject!: (reason?: any) => void;
+				this.connectionPromise = new Promise((res, rej) => {
+					resolve = res;
+					reject = rej;
+				});
+				const loki = new Loki(config.lokiJSfile ?? 'database.json', {
+					autosave: true,
+					autosaveInterval: 1000,
+					autoload: true,
+					autoloadCallback(err) {
+						if (err == null) {
+							resolve();
+						} else {
+							reject(err);
+						}
+					},
+				});
+				this.connection = new LokiJSWrapper(loki);
+				return;
+			}
+		}
+	}
+
+	async onLoad() {
 		console.log(LANG.internal.mongodb.called);
 		try {
-			await mongoose.connect(
-				`mongodb://${config.mongoDBuser}:${config.mongoDBpass}@${config.mongoDBhost}:${config.mongoDBport}/${config.mongoDBdatabase}?authSource=admin`,
-			);
+			await this.connectionPromise;
 		} catch (e) {
 			console.log(e);
 		}
 	}
 
 	async onUnload() {
-		this.connection.close();
+		await this.connection.close();
 	}
 }
 
